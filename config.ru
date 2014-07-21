e9dfaf1971ffe7fd5ede3dd84136a0a8a48c467f
Carlos Alonso <mrcalonsoperez@gmail.com>
2014-07-21 23:00:41 +0100

    CORS via gem

--- config.ru
+++ config.ru
@@ -1,6 +1,14 @@
 
+require 'rack/cors'
 require_relative 'racing'
 
 $stdout.sync = true
+
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [ :get, :post ]
+  end
+end
 
 run Interlat::Racing
--- racing.rb
+++ racing.rb
@@ -7,11 +7,6 @@ Dir.glob('./{config,model,api}/*.rb').each { |file| require file }
 module Interlat
   class Racing < Grape::API
 
-    before do
-      header['Access-Control-Allow-Origin'] = '*'
-      header['Access-Control-Request-Method'] = '*'
-    end
-
     mount ConfigAPI
     mount RankingAPI
     add_swagger_documentation
